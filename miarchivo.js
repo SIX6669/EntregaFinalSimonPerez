61a55210131fed59be22d66a54e863494dd0aafb
SIX <simon.494.perrez@gmail.com>
2024-02-08 11:52:36 -0300

    m

--- miarchivo.js
+++ miarchivo.js
@@ -1,24 +1,44 @@
-let listaDeTareas = [];
-
-function crearTarea(){
-    let contador = listaDeTareas.length + 1;
-
-    while(true){
-       let nuevaTarea = prompt('Ingrese una tarea o presione cancelar');
-    
-        if(nuevaTarea === null || nuevaTarea === ''){
-            break;
-        }
-    
-        listaDeTareas.push({id: contador, nombre: nuevaTarea, completada: false });
-        contador ++;
+class Tarea {
+    constructor(id, nombre, completada){
+        this.id = id;
+        this.nombre = nombre;
+        this.completada = completada;
     }
-    
 }
 
+let listaDeTareas = [];
+
+let inputTarea = document.getElementById("inputTarea");
+let addTaskButton = document.getElementById("addTaskButton");
+let showListButton = document.getElementById("showListButton");
+
+
+inputTarea.addEventListener("keydown", function(event){
+    if (event.key==="Enter") {
+       agrearTarea();
+       event.preventDefault();
+    }
+});
+
+addTaskButton.addEventListener("click", function(){
+    agrearTarea();
+});
+
+function agrearTarea(){
+    listaDeTareas.push(new Tarea(listaDeTareas.length + 1, inputTarea.value, false));
+    inputTarea.value ="";
+}
+
+
 function mostrarLista(){
-    let listaFormateada = listaDeTareas.map(tarea => `${tarea.id} - ${tarea.nombre} (${tarea.completada ? 'Completada' : 'Pendiente'})`).join('\n');
-    alert(listaFormateada);
+  let listaElement = document.getElementById("taskList");
+  listaElement.innerHTML = "";
+
+  listaDeTareas.forEach(t =>{
+    let tareaElement = document.createElement("li");
+    tareaElement.textContent = t.nombre;
+    listaElement.appendChild(tareaElement);
+  })
 }
 
 function tareaMarcada(){
@@ -31,3 +51,15 @@ function tareaMarcada(){
         alert('tarea no encontrada');
     }
 }
+
+function mostrarTareaMarcada(tareaNombre){
+    let tarea = listaDeTareas.find(t => t.nombre === tareaNombre);
+    if(tarea){
+        alert(`La tarea ${tareaNombre} ha sido marcada como completada`);
+    } else {
+        alert("Tarea no encontrada");
+    }
+}
+
+
+showListButton.addEventListener("click",mostrarLista);
