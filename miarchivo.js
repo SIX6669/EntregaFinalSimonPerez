26e6a86c98c8e58c1d3048cc4bdef915986fc442
SIX <simon.494.perrez@gmail.com>
2024-03-09 06:06:56 -0300

    m

--- miarchivo.js
+++ miarchivo.js
@@ -244,3 +244,23 @@ document.getElementById("nextMonth").addEventListener("click",()=>{
     now = now.plus({months: 1});
     updateCalendar();
 });
+
+//noticias API
+let key = "f4fa6b0857f442c4b8391146e91189dc";
+let pais ="ar";
+let url = `https://newsapi.org/v2/top-headlines?country=${pais}&apiKey=${key}`;
+let mostrarNoticias = document.getElementById("noticias");
+
+fetch(url).then((resp)=> resp.json()).then(data => {
+    console.log(data);
+    let noticias = data.articles;
+    noticias.map(function(numero){
+        let div = document.createElement("noticias");
+        div.innerHTML =`<br>
+                        <h1>${numero.title}</h1>
+                        <h2>${numero.description}</h2>
+                        <h3>${numero.url}</h3>`;
+        mostrarNoticias.appendChild(div);
+
+    })
+})
